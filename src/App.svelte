74bcb86839467219fb19f37f9e821349320ae456
nesro <nesro@nesro.cz>
2020-12-12 21:41:40 +0100

    svelte template

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>{name}</h1>
+	<h2>Aplikace pro přeušovaný půst</h2>
+	<p>Work in progress! See the code for more info: <a target="_blank" href="https://github.com/nesro/nejim.cz">https://github.com/nesro/nejim.cz</a></p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
