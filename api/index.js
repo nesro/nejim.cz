91e223e2b8ca5acb8eabbdac7f9ebdd0a3decafb
nesro <nesro@nesro.cz>
2020-12-29 23:26:36 +0100

    api connects to mongo, cleaned gitignore a bit

--- api/index.js
+++ api/index.js
@@ -1,16 +1,24 @@
-const app = require('express')()
+require('dotenv').config()
 const { v4 } = require('uuid')
+const express = require('express')
+const userRouter = require('./routers/user')
+require('./db/mongoose')
+
+const app = express()
+
+app.use(express.json())
+app.use(userRouter)
 
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
+  res.end(`Hdddello! Go to item: <a href="${path}">${path}</a>`)
 })
 
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params
-  res.end(`Item: ${slug}`)
+  res.end(`hehItem: ${slug}`)
 })
 
 module.exports = app
