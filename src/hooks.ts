8e5c776bad055094021b8cf6ef6c78d5e293154a
nesro <nesro@nesro.cz>
2021-11-21 01:01:01 +0100

    revert minify

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,30 +2,30 @@ import cookie from 'cookie';
 import _ from 'lodash';
 import { v4 as uuid } from 'uuid';
 import { ObjectId } from 'mongodb';
-import { minify } from 'html-minifier';
+// import { minify } from 'html-minifier';
 import { getCollection } from '$lib/db';
 import { prerendering } from '$app/env';
 import type User from './models/User';
 import type { Handle } from '@sveltejs/kit';
 
-const minification_options = {
-    collapseBooleanAttributes: true,
-    collapseWhitespace: true,
-    conservativeCollapse: true,
-    decodeEntities: true,
-    html5: true,
-    ignoreCustomComments: [/^#/],
-    minifyCSS: true,
-    minifyJS: true,
-    removeAttributeQuotes: true,
-    removeComments: true,
-    removeOptionalTags: true,
-    removeRedundantAttributes: true,
-    removeScriptTypeAttributes: true,
-    removeStyleLinkTypeAttributes: true,
-    sortAttributes: true,
-    sortClassName: true,
-};
+// const minification_options = {
+//     collapseBooleanAttributes: true,
+//     collapseWhitespace: true,
+//     conservativeCollapse: true,
+//     decodeEntities: true,
+//     html5: true,
+//     ignoreCustomComments: [/^#/],
+//     minifyCSS: true,
+//     minifyJS: true,
+//     removeAttributeQuotes: true,
+//     removeComments: true,
+//     removeOptionalTags: true,
+//     removeRedundantAttributes: true,
+//     removeScriptTypeAttributes: true,
+//     removeStyleLinkTypeAttributes: true,
+//     sortAttributes: true,
+//     sortClassName: true,
+// };
 
 export const handle: Handle = async ({ request, resolve }) => {
     const cookies = cookie.parse(request.headers.cookie || '');
@@ -63,9 +63,9 @@ export const handle: Handle = async ({ request, resolve }) => {
         });
     }
 
-    if (prerendering && response.headers['content-type'] === 'text/html') {
-        response.body = minify(response.body, minification_options);
-    }
+    // if (prerendering && response.headers['content-type'] === 'text/html') {
+    //     response.body = minify(response.body, minification_options);
+    // }
 
     return response;
 };
